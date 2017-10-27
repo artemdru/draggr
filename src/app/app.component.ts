107e20f1069f57f2d2edfec65f4d2786a9d5778e
Artem <artemdr1@gmail.com>
2017-10-27 10:18:18 -0700

    added jquery and scroll OnInit logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'app';
 
   dates: Date[];
+
+  dateWidth: number;
   
   today = new Date();
   leftMostDay = new Date(new Date().setDate(new Date().getDate()-2));
@@ -24,5 +27,18 @@ export class AppComponent implements OnInit {
     this.rightMostDay.setHours(0,0,0,0);
 
     this.dates = [this.leftMostDay, this.leftDay, this.today, this.rightDay, this.rightMostDay];
+
+  }
+
+  ngAfterViewInit() {
+    this.dateWidth = $('#date').width();
+    $('.days-otw').scrollLeft(this.dateWidth);
+    $('.calendar').scrollLeft(this.dateWidth);
+  }
+
+  onResize(){
+    this.dateWidth = $('#date').width();
+    $('.days-otw').scrollLeft(this.dateWidth);
+    $('.calendar').scrollLeft(this.dateWidth);
   }
 }
